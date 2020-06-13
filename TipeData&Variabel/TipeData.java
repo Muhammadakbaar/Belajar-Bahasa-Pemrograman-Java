e9b2c57673f91a53b9a2c120862161b61e870f04
Muhammad Akbar <sitamvan@sitamvan>
2020-06-13 13:07:02 +0700

    Tipedata: add

---
+++ TipeData&Variabel/TipeData.java
@@ -0,0 +1,21 @@
+public class TipeData{
+    public static void main(String [] args){
+        // Bilangan bulat
+        byte a ;
+        short b;
+        int c;
+        Long d;
+
+        // Bilangan pecahan
+        float e;
+        double f;
+
+        // Boolean
+        boolean g;
+        boolean h;
+
+        // Karakter
+        char i;
+
+    }
+}
