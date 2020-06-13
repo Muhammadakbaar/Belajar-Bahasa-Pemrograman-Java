c6c6f8e16839f348aefba007c2f2799f467d2d36
Muhammad Akbar <sitamvan@sitamvan>
2020-06-13 13:12:58 +0700

    Variabel: add

---
+++ TipeData&Variabel/Variabel.java
@@ -0,0 +1,23 @@
+public class Variabel {
+    public static void main(String [] args){
+        // Bilangan bulat
+        byte a = 127;
+        short b = 32767;
+        int c = 2147483647;
+        long d = 922337857;
+
+        // Bilangan pecahan
+        float e = 5.50f;
+        double f = 7.70d;
+
+        // Boolean
+        boolean g = true;
+        boolean h = false;
+
+        // Karakter
+        char i = 'A';
+
+            }
+        
+    }
+    
