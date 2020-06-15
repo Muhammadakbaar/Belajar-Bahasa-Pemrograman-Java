83a9bfb599c5bb46facddf2cd3c05d15057e08bd
Muhammad Akbar <sitamvan@sitamvan>
2020-06-15 17:00:10 +0700

    update if else

---
+++ If else/IfElse.java
@@ -0,0 +1,10 @@
+public class IfElse{
+    public static void main(String [] args){
+        int a = 5;
+        if(a==5){
+            System.out.print("Benar");
+        }else{
+            System.out.println("Salah");
+        }
+    }
+}
