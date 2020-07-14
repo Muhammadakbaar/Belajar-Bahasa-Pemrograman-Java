02a8c373d215c7f8334c4537f4e2e8eeeec82b00
Muhammad Akbar <mohammadakbaar11@gmail.com>
2020-07-15 00:05:14 +0700

    update input user kedua

--- User Input/src/com/praxis/Main.java
+++ User Input/src/com/praxis/Main.java
@@ -5,14 +5,15 @@ import  java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	Scanner input = new Scanner(System.in);
+        Scanner input = new Scanner(System.in);
 
         System.out.println("Masukan nilai pertama = ");
-	    int data1 = input.nextInt();
-        System.out.println("masukan nilai pertama " + data1);
+        int data1 = input.nextInt();
 
+        System.out.println("masukan nilai kedua = ");
         int data2 = input.nextInt();
-        System.out.println("masukan data kedua " + data2);
 
+        int data3 = data1 + data2;
+        System.out.println("Jumlah nilai keduanya adalah = " + data3);
     }
 }
