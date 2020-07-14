bba2840bfe8859e9b9daa89cc4a8af7248382fef
Muhammad Akbar <mohammadakbaar11@gmail.com>
2020-07-15 00:01:09 +0700

    update input user

---
+++ User Input/src/com/praxis/Main.java
@@ -0,0 +1,18 @@
+package com.praxis;
+
+import  java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	Scanner input = new Scanner(System.in);
+
+        System.out.println("Masukan nilai pertama = ");
+	    int data1 = input.nextInt();
+        System.out.println("masukan nilai pertama " + data1);
+
+        int data2 = input.nextInt();
+        System.out.println("masukan data kedua " + data2);
+
+    }
+}
