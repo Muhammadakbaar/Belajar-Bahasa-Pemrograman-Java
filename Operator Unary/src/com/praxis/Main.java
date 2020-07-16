16d1026f77e092fda37be5758fb45f8bf0503f87
Muhammad Akbar <mohammadakbaar11@gmail.com>
2020-07-16 20:13:54 +0700

    update operator unary

--- Operator Unary/src/com/praxis/Main.java
+++ Operator Unary/src/com/praxis/Main.java
@@ -3,6 +3,10 @@ package com.praxis;
 public class Main {
 
     public static void main(String[] args) {
+        // unary = operasi yang dilakukan pada satu variabel
         // ini adalah program untuk operator unary
+
+        int angka = 1;
+        System.out.println(-angka);
     }
 }
