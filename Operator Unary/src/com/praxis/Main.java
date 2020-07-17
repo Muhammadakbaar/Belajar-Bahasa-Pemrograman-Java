b9411eea88836101c9b168b4c94a3671fd20c0cd
Muhammad Akbar <mohammadakbaar11@gmail.com>
2020-07-17 19:14:55 +0700

    update operator unary

--- Operator Unary/src/com/praxis/Main.java
+++ Operator Unary/src/com/praxis/Main.java
@@ -6,7 +6,18 @@ public class Main {
         // unary = operasi yang dilakukan pada satu variabel
         // ini adalah program untuk operator unary
 
+            // unary - dan +
         int angka = 1;
         System.out.printf("unary '-', %d menjadi %d \n",angka,-angka);
+        System.out.printf("unary '+', %d menjadi %d \n",angka,+angka);
+
+        // unary decrement dan increment
+        int angka2 = 10;
+        angka2++;
+        System.out.println("nilai dengan '++' menjadi = " + angka2);
+
+        int angka3 = 10;
+        angka3--;
+        System.out.println("nilai dengan '++' menjadi = " + angka3);
     }
 }
