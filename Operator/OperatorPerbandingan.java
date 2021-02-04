127d259ae703c3e3824116ba17edd940dbb72505
muhammadakbaar <mohammadakbaar11@gmail.com>
2021-02-05 05:09:59 +0700

    biar ijo aja

---
+++ Operator/OperatorPerbandingan.java
@@ -0,0 +1,26 @@
+public class OperatorPerbandingan {
+
+    public static void main(String [] args){
+        int a = 5;
+        int b = 10;
+        
+        boolean c = a == b;
+        boolean d = a != b;
+        boolean e = a < b;
+        boolean f = a > b;
+        boolean g = a <= b;
+        boolean h = a >= b;
+
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(e);
+        System.out.println(f);
+        System.out.println(g);
+        System.out.println(h);
+
+
+
+
+
+    }    
+}
