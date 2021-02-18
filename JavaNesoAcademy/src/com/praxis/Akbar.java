535bbb92fc96905210484c0d690963b2eaeca76d
muhammadakbaar <mohammadakbaar11@gmail.com>
2021-02-19 01:04:47 +0700

    menambahkan beberapa class

---
+++ JavaNesoAcademy/src/com/praxis/Akbar.java
@@ -0,0 +1,12 @@
+package com.praxis;
+
+public class Akbar {
+    public static void main(String[] args) {
+        System.out.println("Sebelum sayHi");
+        sayHi();
+        System.out.println("Sesudah sayHai");
+    }
+    public static void sayHi(){
+        System.out.println("Hallo semuannya");
+    }
+}
---
+++ Mix/oop/Main.java
@@ -0,0 +1,7 @@
+int find_max(int nums[], int len)
+{
+int i, max_num = nums[0];
+or (i = 1; i < len; i ++)
+if (nums[i] > max_num)
+return max_num;
+} 
