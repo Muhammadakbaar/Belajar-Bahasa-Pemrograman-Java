954043985d582a931e42d3dabda182cd0fe6a92e
muhammadakbaar <mohammadakbaar11@gmail.com>
2021-02-20 22:10:56 +0700

    menambahkan beberapa fungsi

--- JavaNesoAcademy/src/com/praxis/Akbar.java
+++ JavaNesoAcademy/src/com/praxis/Akbar.java
@@ -5,4 +5,9 @@ public class Akbar {
     public static void sayHi(){
         System.out.println("Hallo semuannya");
     }
+
+    public void fungsi(){
+        System.out.println("Ini fungsi tanpa static void");
+
+    }
 }
--- JavaNesoAcademy/src/com/praxis/Main.java
+++ JavaNesoAcademy/src/com/praxis/Main.java
@@ -4,6 +4,9 @@ public class Main {
 
     public static void main(String[] args) {
         Akbar.sayHi();
+        Akbar akbar = new Akbar();
+
+        akbar.fungsi();
 
         // public = access modifier di dlam class, luar class, dalam package, luar package
         // private = access modifier hanya didalam class
