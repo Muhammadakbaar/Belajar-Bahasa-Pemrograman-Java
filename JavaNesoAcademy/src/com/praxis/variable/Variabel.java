52636dc93d848429dca3bebca3f1a55559dbdcfa
muhammadakbaar <mohammadakbaar11@gmail.com>
2021-02-23 15:24:21 +0700

    add some class and package

---
+++ JavaNesoAcademy/src/com/praxis/variable/Variabel.java
@@ -0,0 +1,15 @@
+package com.praxis.variable;
+
+public class Variabel {
+    public static void main(String[] args) {
+        int a = 5;
+
+        int b,c;
+        b = 5;
+        c = a + b;
+
+
+        System.out.println(c);
+    }
+
+}
